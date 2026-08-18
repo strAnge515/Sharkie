fae678a3e2bef0aebd64d41970075fe31882acd7
Denny <kleinerumbero@gmail.com>
2026-08-18 21:19:59 +0200

    character class added

---
+++ scripts/models/character.class.js
@@ -0,0 +1,16 @@
+class Character extends MoveableObject {
+
+CHARACTER_IMAGE = [];
+x = 100;
+y = 100;
+height= 50;
+width = 70;
+
+
+    constructor() {
+        super();
+        this.loadImage('graphics/1.Sharkie/1.IDLE/1.png');
+    }
+
+
+}
