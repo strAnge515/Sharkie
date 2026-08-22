f137161e51c7850525832df9eff7e39f2ebca1a7
Denny <kleinerumbero@gmail.com>
2026-08-22 17:40:49 +0200

    light added

--- levels/level1.js
+++ levels/level1.js
@@ -19,6 +19,7 @@ const level1 = new Level(
     new BackgroundObject('graphics/3. Background/Layers/5. Water/D.png', 0),
     new BackgroundObject('graphics/3. Background/Layers/4.Fondo 2/D.png', 0),
     new BackgroundObject('graphics/3. Background/Layers/3.Fondo 1/D.png', 0),
-    new BackgroundObject('graphics/3. Background/Layers/2. Floor/D.png', 0),
+    new BackgroundObject('graphics/3. Background/Layers/1. Light/1.png', 0),
+    new BackgroundObject('graphics/3. Background/Layers/2. Floor/D.png', 0)
   ],
 );
